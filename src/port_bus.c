f70847a91faf524245b19086240d880747bc0512
SapphireAmoeba5 <clevermail19@gmail.com>
2022-09-11 12:31:41 -0300

    Reading from an invalid port writes all ones to the destination

--- src/port_bus.c
+++ src/port_bus.c
@@ -14,8 +14,8 @@ port_bus* port_bus_create() {
 }
 
 void port_bus_destroy(port_bus* port_bus) {
-    for(size_t i = 0; i < 0xffff; i++) {
-        if(port_bus->ports[i] != NULL) {
+    for (size_t i = 0; i < 0xffff; i++) {
+        if (port_bus->ports[i] != NULL) {
             free(port_bus->ports[i]);
         }
     }
@@ -24,7 +24,7 @@ void port_bus_destroy(port_bus* port_bus) {
 }
 
 bool port_bus_add_device(port_bus* port_bus, u16 port, port_bus_write_callback write_callback, port_bus_read_callback read_callback, void* private_data) {
-    if(port_bus->ports[port] != NULL) {
+    if (port_bus->ports[port] != NULL) {
         return false;
     }
 
@@ -42,7 +42,7 @@ bool port_bus_add_device(port_bus* port_bus, u16 port, port_bus_write_callback w
 void port_bus_remove_device(port_bus* port_bus, u16 port) {
     port_bus_entry* e = port_bus->ports[port];
 
-    if(e != NULL) {
+    if (e != NULL) {
         free(e);
     }
 }
@@ -50,7 +50,7 @@ void port_bus_remove_device(port_bus* port_bus, u16 port) {
 void port_bus_write(port_bus* port_bus, u16 port, u64 value) {
     port_bus_entry* e = port_bus->ports[port];
 
-    if(e != NULL && e->write_callback != NULL) {
+    if (e != NULL && e->write_callback != NULL) {
         e->write_callback(value, e->private_data);
     }
 }
@@ -58,7 +58,10 @@ void port_bus_write(port_bus* port_bus, u16 port, u64 value) {
 void port_bus_read(port_bus* port_bus, u16 port, u64* dest) {
     port_bus_entry* e = port_bus->ports[port];
 
-    if(e != NULL && e->read_callback != NULL) {
+    if (e != NULL && e->read_callback != NULL) {
         e->read_callback(dest, e->private_data);
+    }
+    else if (dest != NULL) {
+        *dest = 0xffffffffffffffff;
     }
 }
